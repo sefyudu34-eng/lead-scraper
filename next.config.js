dc2cace825993ce3aa7b3b34ea563be48f166b89
sefyudu34-eng <sefyudu34@gmail.com>
2025-11-16 17:14:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // permet d’exporter en statique si besoin
 };
 
 module.exports = nextConfig;
