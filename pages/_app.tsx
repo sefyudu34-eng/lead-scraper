a65b1ad4a001b0311ee641a1a6172f35582eda7e
sefyudu34-eng <sefyudu34@gmail.com>
2025-11-16 17:13:54 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'; // si tu utilises Tailwind
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
