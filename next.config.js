89368b81e2b36075cc297ad4211f98cee7babdbb
sefyudu34-eng <sefyudu34@gmail.com>
2025-11-16 16:00:21 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // permet d’exporter en statique si besoin
+};
+
+module.exports = nextConfig;
