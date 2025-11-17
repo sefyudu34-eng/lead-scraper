f990b2b1743d2d25ef95e7c51ded1c3a2644352c
sefyudu34-eng <sefyudu34@gmail.com>
2025-11-17 11:39:05 +0100

    Update next.config.js aaaaq

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export' ← supprimé exprès pour que les API routes fonctionnent sur Vercel
+  images: {
+    unoptimized: true // obligatoire quand tu avais output: 'export' avant, on le garde pour éviter les erreurs d'images
+  }
 };
 
 module.exports = nextConfig;
